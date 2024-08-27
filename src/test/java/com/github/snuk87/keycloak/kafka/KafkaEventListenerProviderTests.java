cdb5140d7f24d870ba532d98f24bc38935997a0b
Iulia Ratiu <ratiuiulia@yahoo.com>
2024-08-27 16:20:55 +0300

    [OS-396] Update KafkaEventListenerProviderTests

--- src/test/java/com/github/snuk87/keycloak/kafka/KafkaEventListenerProviderTests.java
+++ src/test/java/com/github/snuk87/keycloak/kafka/KafkaEventListenerProviderTests.java
@@ -2,76 +2,93 @@ package com.github.snuk87.keycloak.kafka;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.lang.reflect.Field;
-import java.util.Map;
-
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Collections;
 import org.apache.kafka.clients.producer.MockProducer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.mockito.Mockito;
+
+
 
 class KafkaEventListenerProviderTests {
 
 	private KafkaEventListenerProvider listener;
-	private KafkaProducerFactory factory;
+	private KeycloakSessionHelper mockSessionHelper;
+	private KafkaConfigService kafkaConfigService;
+	private KafkaProducerInitializer kafkaProducerInitializer;
+	private MockProducer<String, String> mockProducer;
+	//private Map<String, Object> kafkaProducerProperties = new HashMap<>();
 
 	@BeforeEach
 	void setUp() throws Exception {
-		factory = new KafkaMockProducerFactory();
-		listener = new KafkaEventListenerProvider("", "", "", new String[] { "REGISTER" }, "admin-events", Map.of(),
-				factory);
+		mockSessionHelper= Mockito.mock(KeycloakSessionHelper.class);
+		kafkaConfigService= Mockito.mock(KafkaConfigService.class);
+		kafkaProducerInitializer= Mockito.mock(KafkaProducerInitializer.class);
+
+		mockProducer = new MockProducer<>(
+				true,                   // autoComplete
+				new StringSerializer(), // keySerializer
+				new StringSerializer()  // valueSerializer
+		);
+
+		Mockito.when(kafkaProducerInitializer.GetKafkaProducerByRealmName(Mockito.anyString())).thenReturn(mockProducer);
+		Mockito.when(mockSessionHelper.GetRealmName(Mockito.anyString())).thenReturn("realmTest");
+		Mockito.when(kafkaConfigService.getEvents()).thenReturn(new String[]{"REGISTER", "LOGIN"});
+		Mockito.when(kafkaConfigService.getTopicAdminEvents()).thenReturn("event");
+		Mockito.when(kafkaProducerInitializer.GetKafkaTopicsByRealmName(Mockito.anyString())).thenReturn(Collections.singletonList("testTopic"));
+
+
+		listener = new KafkaEventListenerProvider(mockSessionHelper,kafkaConfigService, kafkaProducerInitializer);
 	}
 
 	@Test
 	void shouldProduceEventWhenTypeIsDefined() throws Exception {
 		Event event = new Event();
 		event.setType(EventType.REGISTER);
-		MockProducer<?, ?> producer = getProducerUsingReflection();
 
 		listener.onEvent(event);
 
-		assertEquals(1, producer.history().size());
+		assertEquals(1, mockProducer.history().size());
 	}
 
 	@Test
 	void shouldDoNothingWhenTypeIsNotDefined() throws Exception {
 		Event event = new Event();
 		event.setType(EventType.CLIENT_DELETE);
-		MockProducer<?, ?> producer = getProducerUsingReflection();
 
 		listener.onEvent(event);
 
-		assertTrue(producer.history().isEmpty());
+		assertTrue(mockProducer.history().isEmpty());
 	}
 
 	@Test
 	void shouldProduceEventWhenTopicAdminEventsIsNotNull() throws Exception {
 		AdminEvent event = new AdminEvent();
-		MockProducer<?, ?> producer = getProducerUsingReflection();
 
 		listener.onEvent(event, false);
 
-		assertEquals(1, producer.history().size());
+		assertEquals(1, mockProducer.history().size());
 	}
 
 	@Test
 	void shouldDoNothingWhenTopicAdminEventsIsNull() throws Exception {
-		listener = new KafkaEventListenerProvider("", "", "", new String[] { "REGISTER" }, null, Map.of(), factory);
+		listener = new KafkaEventListenerProvider(mockSessionHelper,kafkaConfigService, kafkaProducerInitializer);
 		AdminEvent event = new AdminEvent();
-		MockProducer<?, ?> producer = getProducerUsingReflection();
+		Mockito.when(kafkaConfigService.getTopicAdminEvents()).thenReturn(null);
 
 		listener.onEvent(event, false);
 
-		assertTrue(producer.history().isEmpty());
+		assertTrue(mockProducer.history().isEmpty());
 	}
 
-	private MockProducer<?, ?> getProducerUsingReflection() throws Exception {
-		Field producerField = KafkaEventListenerProvider.class.getDeclaredField("producer");
+	/*private Map<String,MockProducer<?, ?>> getProducerUsingReflection() throws Exception {
+		Field producerField = KafkaProducerInitializer.class.getDeclaredField("kafkaProducers");
 		producerField.setAccessible(true);
-		return (MockProducer<?, ?>) producerField.get(listener);
-	}
+		return (Map<String,MockProducer<?, ?>>) producerField.get(listener);
+	}*/
 
 }
