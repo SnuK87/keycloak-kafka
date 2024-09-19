4de966803ee1a2b5840cbc0eb6139e213070fdc6
Timot Tarjani <ttimotster@gmail.com>
2024-09-19 06:26:45 +0000

    Revert the key serializer

--- src/main/java/com/github/snuk87/keycloak/kafka/KafkaStandardProducerFactory.java
+++ src/main/java/com/github/snuk87/keycloak/kafka/KafkaStandardProducerFactory.java
@@ -7,6 +7,7 @@ import com.github.snuk87.keycloak.kafka.serializer.JsonSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +21,7 @@ public final class KafkaStandardProducerFactory implements KafkaProducerFactory 
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
 		props.putAll(optionalProperties);
 
