1f8eee9d4685acde5c7bba0b4a790ff92b6b13c4
Dennis Griese <d.griese1987@gmail.com>
2025-11-27 22:06:06 +0100

    update dependencies

--- src/test/java/com/github/snuk87/keycloak/kafka/KafkaMockProducerFactory.java
+++ src/test/java/com/github/snuk87/keycloak/kafka/KafkaMockProducerFactory.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.RoundRobinPartitioner;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 class KafkaMockProducerFactory implements KafkaProducerFactory {
@@ -11,7 +12,7 @@ class KafkaMockProducerFactory implements KafkaProducerFactory {
 	@Override
 	public Producer<String, String> createProducer(String clientId, String bootstrapServer,
 			Map<String, Object> optionalProperties) {
-		return new MockProducer<>(true, new StringSerializer(), new StringSerializer());
+		return new MockProducer<>(true, new RoundRobinPartitioner(), new StringSerializer(), new StringSerializer());
 	}
 
 }
